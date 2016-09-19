06aa501618cc81b7c3ddf520c398e592fe92ade2
kjgilbert <kgilbert@zoology.ubc.ca>
2016-09-19 13:29:00 -0400

    technically working and correct, but checking on actual values which are maybe weird

--- MeanFitness.R
+++ MeanFitness.R
@@ -7,87 +7,64 @@
 # so use full output at end
 
 
+calc.fitness <- function(full.poly.muts.dat, full.genomes.dat, fixed.mut.dat, pop.size){
 
-# this is a full output file, that was exported to the file name of my choosing, so it has the sections that can be subset, but does not need trimming of other outputs or run info
-
-file <- "~/Documents/My_Documents/UofToronto/SLiM/Outputs/Old_Outputs/Test_SmallerN/out_Aug16_N5000_deletOnly_partRec_outcr_gen8N-full.txt"
-
-# find mutation IDs and their effects
-## mut.id.start <- "Mutations:"
-mut.id.start <- as.numeric(strsplit(system(paste(c("grep -n Mutations ", file), collapse=""), intern=TRUE), split=":")[[1]][1])
-
-## inds.start <- "Individuals:"
-inds.start <- as.numeric(strsplit(system(paste(c("grep -n Individuals ", file), collapse=""), intern=TRUE), split=":")[[1]][1])
-
-## genomes.start <- "Genomes:"
-genomes.start <- as.numeric(strsplit(system(paste(c("grep -n Genomes ", file), collapse=""), intern=TRUE), split=":")[[1]][1])
-
-file.end <- as.numeric(strsplit(system(paste(c("wc -l ", file), collapse=""), intern=TRUE), split=" ")[[1]][1])
-
-
-#read all lines of the data file in
-## dat <- readLines(file)
-
-
-
-#take only the lines starting 1 line after "Mutations:" and up to 1 line before "Individuals"
-## mutdat <- dat[(which(dat == mut.id.start)+1):(which(dat == inds.start)-1)]
-mutdat <- read.table(file, skip=mut.id.start, nrow=((inds.start-1) - mut.id.start), sep=" ")
-names(mutdat) <- c("mut.ID", "mut.type", "base.pos", "seln.coef", "dom.coef", "pop.ID", "gen.ID", "mut.prev")
-# columns =
-#	(1) a unique identifying ID, 
-#	(2) the mutation type, 
-#	(3) the base position, 
-#	(4) the selection coefficient (here always 0 since this is a neutral model), 
-#	(5) the dominance coefficient (here always 0.5), 
-#	(6) the identifier of the subpopulation in which the mutation first arose, 
-#	(7) the generation in which it arose, and 
-#	(8) the prevalence of the mutation (the number of genomes that contain the mutation, where – in the way that SLiM uses the term “genome” – there are two genomes per individual)
-
-
-#take only the lines starting 1 line after "Individuals:" and up to 1 line before "Genomes"
-## inddat <- dat[(which(dat == inds.start)+1):(which(dat == genomes.start)-1)]
-#		inddat <- read.table(file, skip=inds.start, nrow=((genomes.start-1) - inds.start), sep=" ")
-#		names(inddat) <- c("pop.ind", "sex", "genome.1", "genome.2")
-# don't really need this info, it's redundant
-
-
-#take only the lines starting 1 line after "Genomes:" and up to end of file
-## gendat <- dat[(which(dat == mut.id.start)+1):length(dat)]
-gendat <- read.table(file, skip=genomes.start, sep="A")
-names(gendat) <- c("pop.genome", "muts")
-
-
-# loop through every individual, i.e. every pair
-# get its fitness based on the diploid combination of mutations it has
-pop.size <- 5000
-
-fitness.results <- data.frame(matrix(NA, ncol=2))
-names(fitness.results) <- c("individual", "fitness")
-
-iterate.inds <- 1
-for(i in seq(1, (pop.size*2), by=2)){
-	genome.1.muts <- unlist(strsplit(as.character(gendat$muts[i]), split=" "))[-1]
-	genome.2.muts <- unlist(strsplit(as.character(gendat$muts[(i+1)]), split=" "))[-1]
-
-	# mutations both chromosomes have make them homozygotes for that mutation
-	hom.muts <- which(genome.1.muts %in% genome.2.muts)
-	# mutations only one chromosome has make them heterozygotes for that mutation
-	het.muts <- c(which(!(genome.1.muts %in% genome.2.muts)), which(!(genome.2.muts %in% genome.1.muts)))
-	# no need/no way to detect wild type homozygots, so don't need to worry about for fitness
+	fitness.results <- data.frame(matrix(NA, ncol=2))
+	names(fitness.results) <- c("individual", "poly.mut.fitness")	
 	
-	ind.fitness <- 1
-	# match up homozygous mutations to fitness effects - homozygous = s
-	for(j in hom.muts){
-		ind.fitness <- ind.fitness + mutdat$seln.coef[mutdat$mut.ID == j]
-	}
-	# match up heterozygous mutations to fitness effects - heterozygous = hs
-	for(j in het.muts){
-		ind.fitness <- ind.fitness + (mutdat$seln.coef[mutdat$mut.ID == j] * mutdat$dom.coef[mutdat$mut.ID == j])
+	genome.data <- lapply(as.character(full.genomes.dat[,2]), FUN=strsplit, split=" ")
+
+	# go through every two haploid genomes to get every diploid inds fitness
+	iterate.inds <- 1
+	for(i in seq(1, (pop.size*2), by=2)){
+		genome.1.muts <- unlist(genome.data[[1]])[-1]
+		genome.2.muts <- unlist(genome.data[[i+1]])[-1]
+	
+		# mutations both chromosomes have make them homozygotes for that mutation
+		hom.muts <- genome.1.muts[which(genome.1.muts %in% genome.2.muts)]
+		# mutations only one chromosome has make them heterozygotes for that mutation
+		het.muts <- genome.1.muts[c(which(!(genome.1.muts %in% genome.2.muts)), which(!(genome.2.muts %in% genome.1.muts)))]
+		# no need/no way to detect wild type homozygotes, so don't need to worry about for fitness
+	
+		
+		## fitness is multiplicative:
+		# remove neutral mutations because that will mess up the product
+		hom.fitness <- prod(1 + full.poly.muts.dat[which(full.poly.muts.dat$mut.ID %in% hom.muts) ,]$seln_coeff[full.poly.muts.dat[which(full.poly.muts.dat$mut.ID %in% hom.muts) ,]$seln_coeff !=0])
+		het.fitness <- prod(1 + (full.poly.muts.dat[which(full.poly.muts.dat$mut.ID %in% het.muts) ,]$seln_coeff[full.poly.muts.dat[which(full.poly.muts.dat$mut.ID %in% het.muts) ,]$seln_coeff !=0] * full.poly.muts.dat[which(full.poly.muts.dat$mut.ID %in% het.muts) ,]$dom_coeff[full.poly.muts.dat[which(full.poly.muts.dat$mut.ID %in% het.muts) ,]$seln_coeff !=0]))
+	##	and take that value within this loop and save it, so that when getting decriment from fixed muts, can multiply together first then subtract from 1
+				
+		fitness.results[iterate.inds,] <- c(iterate.inds, (hom.fitness * het.fitness))
+		iterate.inds <- iterate.inds + 1
 	}
 	
-	fitness.results[iterate.inds,] <- c(iterate.inds, ind.fitness)
-	iterate.inds <- iterate.inds + 1
+	# the above is all for polymorphic mutations
+	
+	# now need to include in individual fitness the effect of all fixed mutations
+	#	(all fixed muts are always present at the last full generation time point sampled)
+	fixed.fitness <- prod(1 + fixed.mut.dat$seln_coeff[fixed.mut.dat$seln_coeff != 0])	# can't be right either because gives zero - so clearly all the realy neutral ones are the ones that are fixed, but they contribute nothing to fitness loss? Can't multiply them with the others because comes out to zero
+
+	fitness.results$ind.fitness <- fixed.fitness * fitness.results$poly.mut.fitness
+	
+	
+	mean.fitness.poly <- mean(fitness.results$poly.mut.fitness)
+	var.fitness.poly <- var(fitness.results$poly.mut.fitness)
+	mean.fitness.total <- mean(fitness.results$ind.fitness)
+	var.fitness.total <- var(fitness.results$ind.fitness)
+	
+	return(c(mean.fitness.poly, var.fitness.poly, mean.fitness.total, var.fitness.total))
 }
+
+
+
+##	setwd("~/Documents/My_Documents/UofToronto/SLiM/AnalysisScripts")
+
+##	generation <- 100000
+##	num.inds.sampled <- 100
+##	full.poly.muts.dat <- read.table("test_FULLfinal_polymuts.dat")
+##	full.genomes.dat <- read.table("test_FULLfinal_genomes.dat", sep="A")
+##	fixed.mut.dat <- read.table("test_fixedmuts_allgens.dat")
+
+##	names(full.poly.muts.dat) <- c("mut.ID", "mut.type", "base_position", "seln_coeff", "dom_coeff", "subpop_ID", "generation_arose", "mut.prev")
+##	names(fixed.mut.dat) <- c("mut.ID", "mut.type", "base_position", "seln_coeff", "dom_coeff", "subpop_ID", "gen_arose", "gen.fixed")
 
 
