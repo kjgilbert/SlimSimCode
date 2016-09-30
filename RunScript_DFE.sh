b16da587dcdf00207f29b4b380f570afd7467a88
kjgilbert <kgilbert@zoology.ubc.ca>
2016-09-30 17:34:34 -0400

    working up to production of sfs inputs

--- RunScript_DFE.sh
+++ RunScript_DFE.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+# run DFE-alpha on Slim outputs
+
+# for 1 individual slim output, need to:
+#	calculate neutral and selected SFS
+#	make config file
+#	run est_dfe class 0
+#	run est_dfe class 1 with output from class 0
+#	run prop_muts_in_s_range on class 1 output
+
+# if beneficials, run alpha_omega on est_dfe output class 0 and 1
+
+
+# get all outputs in dir to run analyses on
+echo "Provide the path to the directory containing the SLiM outputs to be analysed."
+read dir
+
+# put all those file names in a list
+ls $dir | grep Fixed | sed "s/FixedOutput_//g" > base_InputNames.txt
+
+# loop through the list and do each one at a time
+for base_name in `cat base_InputNames.txt`
+do
+Rscript CommandLine_RunSlimToDFEconversion.R $base_name subsample $dir
+done
+
+# now all the SFS inputs for DFE are ready
+# go through each and do the DFE analyses
+
+###for 
+# make a config file
